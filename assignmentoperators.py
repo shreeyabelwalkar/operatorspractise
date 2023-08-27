baae0805bd53a35dcbc819ac3c5f353aa3152d4c
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-27 16:27:56 +0530

    New file assignmentoperators.py is created

---
+++ assignmentoperators.py
@@ -0,0 +1,19 @@
+x=20
+y=10
+z=5
+#z=x+y
+#print(z) 30
+#z+=x
+#print(z) 25
+#z-=x
+#print(z) -15
+#z*=x
+#print(z)  100
+#z/=x
+#print(z) 0.25
+#z%=x
+#print(z)  5
+#z**=y
+#print(z)   9765625
+#z//=y
+#print(z)  0
