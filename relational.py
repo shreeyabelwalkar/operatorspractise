91001175e9092a610bfa2ab209c9666c9a84f7f8
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-27 16:40:56 +0530

    New file relational.py is created

---
+++ relational.py
@@ -0,0 +1,36 @@
+a=10
+b=20
+print(a>b)
+
+a=10
+b=20
+print(a>=b)
+
+a=10
+b=20
+print(a<b)
+
+a=10
+b=20
+print(a<=b)
+
+a=10
+b=20
+print(a==b)
+
+a=10
+b=20
+print(a!=b)
+
+a=10
+b=20
+if(a>b):
+    print("yes")
+else:
+    print("no")
+
+x=15
+print(10<x<20)
+
+x=15
+print(10<x>20)
