c14bebeb5484a03cba205bcf8574492e6e2b8477
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-27 17:08:33 +0530

    New file membership.py is created

---
+++ membership.py
@@ -0,0 +1,11 @@
+list=[1,2,3,4,5,6,7,8,9]
+if 4 in list:
+    print("present")
+else:
+    print("not present")
+
+list=[1,2,3,4,5,6,7,8,9]
+if 4 not in list:
+    print("not present")
+else:
+    print("present")
