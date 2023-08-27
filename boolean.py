d209ea84adc537399952d51594f0903d5574ff6c
shreeyabelwalkar <shreeya.belwalkar16@gmail.com>
2023-08-27 16:54:57 +0530

    boolean.py is modified

---
+++ boolean.py
@@ -0,0 +1,33 @@
+x=True
+y=False
+print(x and y)
+
+x=True
+y=False
+print(x and x)
+
+x=True
+y=False
+print(y and y)
+
+x=True
+y=False
+print(x or y)
+
+x=True
+y=False
+print(x or x)
+
+x=True
+y=False
+print(y or y)
+
+x=True
+y=False
+print(not x)
+
+x=True
+y=False
+print(not y)
+
+
